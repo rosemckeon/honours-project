37539b7f35daccd103577a31b276ada2c013f01a
Rose <rosemckeon@gmail.com>
2020-03-21 18:49:30 +0000

    worked out transition matrix #10

---
+++ scripts/transition-data.R
@@ -0,0 +1,42 @@
+# from @Peterson2016
+mimulus <- tibble(
+  year = c(rep(2012, 7), rep(2013, 7)),
+  group = as.factor(rep(c("Annuals", "Perennials", "Low-elevation perennials", "Montane perennials", "Silver Fork", "Silver Creek", "Eagle Meadows"), 2)),
+  G = c(0.732, 0.549, 0.652, 0.478, 0.552, 0.410, 0.469, 0.732, 0.549, 0.652, 0.478, 0.552, 0.410, 0.469),
+  O = c(363, 662, 621, 690, 803, 650, 614, 157, 523, 494, 542, 635, 525, 479),
+  F = c(8.59, 3.12, 6.88, 0.56, 0.40, 0.63, 0.64, 4.91, 2.27, 5.34, 0.28, 0.29, 0.27, 0.29),
+  S = c(0, 0.152, 0.162, 0.145, 0.100, 0.158, 0.179, 0, 0.052, 0, 0.086, 0.048, 0.083, 0.122),
+  R = c(0, 1.03, 1.04, 1.02, 0.82, 0.66, 1.56, 0, 0.19, 0, 0.32, 0.17, 0.46, 0.31),
+  n = c(239, 197, 80, 117, 40, 38, 39, 240, 199, 77, 122, 38, 42, 42)
+)
+
+means <- mimulus %>% 
+  dplyr::select(-year, -group, -n) %>%
+  dplyr::summarise_all(mean) %>%
+  dplyr::mutate(n = sum(mimulus$n))
+
+A <- 6.7e-4
+G <- means$G
+O <- means$O
+F <- means$F
+S <- means$S
+R <- means$R
+
+# from @Elderd2006
+D <- 0.534
+
+# based on @Peterson2016 Matrix 1
+trans <- matrix(
+  c(
+    D * (1 - G),
+    D * G,
+    0,
+    F * O * A * (1 - G),
+    F * O * A * G,
+    S * R,
+    F * O * A * (1 - G),
+    F * O * A * G,
+    S * R
+  ), 
+  ncol = 3, nrow = 3
+)
