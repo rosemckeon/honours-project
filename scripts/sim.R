c52d9f563542cd3e2fd396c40270bea233337c29
Rose <rosemckeon@gmail.com>
2020-04-05 10:03:37 +0100

    updates to #16 so that random sims run for around 6rs and each simulation uses a diffrent ploidy_rate

--- scripts/sim.R
+++ scripts/sim.R
@@ -2,7 +2,7 @@ rm(list=ls())
 library(tidyverse)
 library(magrittr)
 library(disturploidy)
-devtools::install_github("rosemckeon/honours-project", auth_token = "60bcd5244537f523313ffae0b061b4ba00873f5d")
+#devtools::install_github("rosemckeon/honours-project", auth_token = "60bcd5244537f523313ffae0b061b4ba00873f5d")
 library(sploidy)
 
 # create a range of ploidy rates with more values close to min than max (following exponential distribution).
@@ -13,26 +13,29 @@ ploidy_rates <- runif(
 ) %>%
   exp()
 
+# pull in the data
 mimulus <- readRDS("scripts/data/mimulus.RDS") %>%
   dplyr::filter(group == "Low-elevation perennials") %>%
   dplyr::filter(year == 2013)
 
-# run null simulations
-sploidy(
-  pop_size = c(20, 20, 20),
-  grid_size = 100,
-  simulations = 1,
-  generations = 1000,
-  ploidy_rate = sample(ploidy_rates, 1, F),
-  name = "_NULL",
-  trans = mimulus %>%
-    dplyr::pull(matrix) %>%
-    magrittr::extract2(1),
-  D = 0.534,
-  G = mimulus %>%
-    dplyr::pull(G),
-  seed_longevity = 0
-)
+# set a roughly 6 hr process going to run the simulations
+# each named randomly with a random ploidy_rate
+for(rate in sample(ploidy_rates, 20, F)){
+  sploidy(
+    pop_size = c(20, 20, 20),
+    grid_size = 100,
+    simulations = 1,
+    generations = 1000,
+    ploidy_rate = rate,
+    trans = mimulus %>%
+      dplyr::pull(matrix) %>%
+      magrittr::extract2(1),
+    D = 0.534,
+    G = mimulus %>%
+      dplyr::pull(G),
+    seed_longevity = 0
+  )
+}
 
 # make sure console message print to console if a simulation has benn interupted by an error
-sink(type = "message")
+#sink(type = "message")
