d54f5871337a3d511b5238b2696881dc875b6c55
Rose <rosemckeon@gmail.com>
2020-04-14 13:32:52 +0100

    @bradduthie here's the tweaked script for #33. Please run as many as possible! I've put a null and parameter test in the same loop so we end up with an equal number of null tests and paremeter changes. (is that a better way to go about it?). I've also left a germination benefit test commented out. I doesn't look like this is doing much interesting so far and I wasn't sure if it might take too long to get results -- doing all three simulations in the script would take about an hour on my machine if they all survived to 1000 gens. Feel free to turn it on if you think there's enough time/processing power to make it work.

--- scripts/sim.R
+++ scripts/sim.R
@@ -5,28 +5,48 @@ library(disturploidy)
 devtools::install_github("rosemckeon/honours-project", auth_token = "60bcd5244537f523313ffae0b061b4ba00873f5d")
 library(sploidy)
 
-# create a range of ploidy rates with more values close to min than max (following exponential distribution).
-ploidy_rates <- runif(
-  n = 100000, 
-  min = log(0.001), 
-  max = log(0.01)
-) %>%
-  exp()
+# create a range of ploidy rates 
+ploidy_rates <- runif(100000, 0.15, 0.25)
 
-# pull in the data
+# create a range of germination modifiers
+G_modifiers <- runif(100000, 1, 1.5)
+
+# create a range of starting population sizes
+N <- 1:100000
+
+# pull in transition the data
 mimulus <- readRDS("scripts/data/mimulus.RDS") %>%
   dplyr::filter(group == "Low-elevation perennials") %>%
   dplyr::filter(year == 2013)
 
-# set a roughly 6 hr process going to run the simulations
-# each named randomly with a random ploidy_rate
-for(rate in sample(ploidy_rates, 20, F)){
+# roughly a 10 hr process
+for(rate in sample(ploidy_rates, 12, F)){
+  #  get a starting pop size
+  n <- sample(N, 1) 
+  # do a control test
   sploidy(
-    pop_size = c(20, 20, 20),
+    pop_size = c(n, n, 0),
     grid_size = 100,
     simulations = 1,
     generations = 1000,
-    filepath = "data/_ploidy_rate_0.001-0.010",
+    filepath = "data/results",
+    ploidy_rate = 0,
+    trans = mimulus %>%
+      dplyr::pull(matrix) %>%
+      magrittr::extract2(1),
+    D = 0.534,
+    G = mimulus %>%
+      dplyr::pull(G),
+    G_modifier = 1,
+    seed_longevity = 0
+  )
+  # and a ploidy_rate test for that same starting N
+  sploidy(
+    pop_size = c(n, n, 0),
+    grid_size = 100,
+    simulations = 1,
+    generations = 1000,
+    filepath = "data/results",
     ploidy_rate = rate,
     trans = mimulus %>%
       dplyr::pull(matrix) %>%
@@ -34,8 +54,26 @@ for(rate in sample(ploidy_rates, 20, F)){
     D = 0.534,
     G = mimulus %>%
       dplyr::pull(G),
+    G_modifier = 1,
     seed_longevity = 0
   )
+  # and a germination benefit test for the same rate and starting N
+  # sploidy(
+  #   pop_size = c(n, n, 0),
+  #   grid_size = 100,
+  #   simulations = 1,
+  #   generations = 1000,
+  #   filepath = "data/results",
+  #   ploidy_rate = rate,
+  #   trans = mimulus %>%
+  #     dplyr::pull(matrix) %>%
+  #     magrittr::extract2(1),
+  #   D = 0.534,
+  #   G = mimulus %>%
+  #     dplyr::pull(G),
+  #   G_modifier = sample(G_modifiers, 1),
+  #   seed_longevity = 0
+  # )
 }
 
 # make sure console message print to console if a simulation has benn interupted by an error
