7103ccf8a3d912ca2f39a0badf39455eeef94e8f
Rose McKeon <rose@rosemckeon.co.uk>
2020-04-28 16:01:22 +0000

    finished results

--- thesis/results.R
+++ thesis/results.R
@@ -1,7 +1,7 @@
 # objects needed for knitting results.Rmd into thesis_2417024.Rmd
-rm(list=ls())
-`%notin%` <- Negate(`%in%`)
-library(tidyverse)
+#rm(list=ls())
+#`%notin%` <- Negate(`%in%`)
+#library(tidyverse)
 # split the trimmed sim data into groups that all have nrow number of sims
 # successful stable simulations
 stability <- simulations %>%
@@ -31,104 +31,114 @@ extinctions <- simulations[-which(simulations$ID %in% success), ] %>%
   dplyr::group_by(ID, ploidy_rate, starting_N) %>%
   dplyr::summarise(gen_extinction_occurred = max(gen))
 
+outcomes <- dplyr::bind_rows(
+  control %>%
+    tidyr::unnest(cols = c(data)) %>%
+    dplyr::filter(adults > 0.99) %>%
+    dplyr::summarise(gen_reached = min(gen)) %>%
+    dplyr::mutate(outcome = "control"),
+  extinctions %>%
+    dplyr::mutate(outcome = "total extinction") %>%
+    dplyr::rename(gen_reached = gen_extinction_occurred),
+  simulations %>%
+    filter(ID %in% stability$ID) %>%
+    unnest(cols = c(data)) %>%
+    filter(adults > 0.99) %>%
+    group_by(ID, ploidy_rate, starting_N) %>%
+    summarise(gen_reached = min(gen)) %>%
+    mutate(outcome = "stable coexistence"),
+  simulations %>%
+    filter(ID %in% unstable_polyploid_winners$ID) %>%
+    unnest(cols = c(data)) %>%
+    filter(polyploid_adults < 0.99, diploid_adults == 0) %>%
+    group_by(ID, ploidy_rate, starting_N) %>%
+    summarise(gen_reached = min(gen)) %>%
+    mutate(outcome = "unstable polyploid fixation"),
+  simulations %>%
+    filter(ID %in% stable_polyploid_winners$ID) %>%
+    unnest(cols = c(data)) %>%
+    filter(polyploid_adults > 0.99, diploid_adults == 0) %>%
+    group_by(ID, ploidy_rate, starting_N) %>%
+    summarise(gen_reached = min(gen)) %>%
+    mutate(outcome = "stable polyploid fixation")
+)
+
+outcomes$outcome <- factor(
+  outcomes$outcome,
+  levels = c(
+    "control",
+    "stable coexistence",
+    "total extinction",
+    "unstable polyploid fixation",
+    "stable polyploid fixation"
+  )
+)
+
+nonreduction <- tibble::tibble(
+  ploidy_rate = seq(0, 0.5, length.out = 1000)
+)
+
+polyploid_winners <- dplyr::bind_rows(
+  simulations %>%
+    dplyr::filter(ID %in% stable_polyploid_winners$ID) %>%
+    dplyr::mutate(outcome = "stable polyploid fixation"),
+  simulations %>%
+    dplyr::filter(ID %in% unstable_polyploid_winners$ID) %>%
+    dplyr::mutate(outcome = "unstable polyploid fixation")
+) %>%
+  tidyr::unnest(cols = c(data)) %>%
+  dplyr::mutate(
+    viable_polyploid_adults = polyploid_adults - sterile_polyploid_adults
+  ) %>%
+  group_by(ID, ploidy_rate, starting_N, outcome) %>%
+  nest()
+
+# simulations %>%
+#   unnest(cols = c(data)) %>%
+#   dplyr::mutate(
+#     viable_polyploid_adults = polyploid_adults - sterile_polyploid_adults
+#   ) %>%
+#   filter(between(viable_polyploid_adults, 0.4, 0.6), gen < 50) %>%
+#   pull(gen) %>% mean()
+#
+# simulations %>%
+#   unnest(cols = c(data)) %>%
+#   filter(between(sterile_polyploid_adults, 0.4, 0.6), gen < 50) %>%
+#   pull(gen) %>% mean()
+
+# over_464 <- simulations %>%
+#   tidyr::unnest(cols = c(data)) %>%
+#   dplyr::mutate(viable_polyploid_adults = polyploid_adults - sterile_polyploid_adults) %>%
+#   dplyr::filter(ploidy_rate > 0.464) %>%
+#   group_by(ID, ploidy_rate, starting_N) %>%
+#   nest()
+#
+# which(over_464$ID %in% extinctions$ID) %>% length()
+
 # exploratory plots -----
 # all the sims
 
-# 
+#
 # simulations %>%
 #   dplyr::filter(ID %in% extinctions$ID) %>%
 #   tidyr::unnest(cols = c(data)) %>%
 #   tidyr::gather(subset, count, diploid_adults:sterile_polyploid_adults) %>%
 #   dplyr::filter(count > 0) %>%
-#   ggplot(aes(x = gen, y = count, colour = subset)) + 
+#   ggplot(aes(x = gen, y = count, colour = subset)) +
 #   geom_point(alpha = .2, size = .2)
-# 
+#
 # simulations %>%
 #   dplyr::filter(ID %in% stable_polyploid_winners$ID) %>%
 #   tidyr::unnest(cols = c(data)) %>%
 #   tidyr::gather(subset, count, diploid_adults:sterile_polyploid_adults) %>%
 #   dplyr::filter(count > 0) %>%
-#   ggplot(aes(x = gen, y = count, colour = subset)) + 
+#   ggplot(aes(x = gen, y = count, colour = subset)) +
 #   geom_point(alpha = .2, size = .2)
-# 
+#
 # simulations %>%
 #   dplyr::filter(ID %in% unstable_polyploid_winners$ID) %>%
 #   tidyr::unnest(cols = c(data)) %>%
 #   tidyr::gather(subset, count, diploid_adults:sterile_polyploid_adults) %>%
 #   dplyr::filter(count > 0) %>%
-#   ggplot(aes(x = gen, y = count, colour = subset)) + 
+#   ggplot(aes(x = gen, y = count, colour = subset)) +
 #   geom_point(alpha = .2, size = .2)
-
-
-
-# the rest ------
-cost <- simulations %>% 
-  dplyr::summarise(count = max(polyploid_adults)) %>%
-  dplyr::mutate(diff = count/ploidy_rate) %>%
-  dplyr::arrange(diff)
-
-cost_summary <- cost %>%
-  dplyr::ungroup() %>%
-  dplyr::mutate(ploidy_rate = round(ploidy_rate, 2)) %>%
-  dplyr::group_by(ploidy_rate) %>%
-  dplyr::summarise(
-    mean = mean(diff),
-    sd = sd(diff),
-    lower = mean - sd * 1.96,
-    upper = mean + sd * 1.96
-  )
-
-range <- counts %>%
-  dplyr::select(-seeds, -adults, -diploid_seeds, - polyploid_seeds, -total) %>%
-  dplyr::filter(between(ploidy_rate, 0.001, .25)) %>%
-  tidyr::gather(
-    "subset", "count",
-    diploid_adults:polyploid_adults
-  )
-
-range <- range %>%
-  dplyr::mutate(
-    panel = "whatever",
-    panel = replace(
-      panel, which(round(range$ploidy_rate, 3) <= 0.171),
-      "Below threshold"
-    ),
-    panel = replace(
-      panel, which(round(range$ploidy_rate, 3) > 0.171),
-      "Over threshold"
-    )
-  )
-
-
-
-counts %>%
-  tidyr::gather(
-    "subset", "count", 
-    diploid_adults:polyploid_adults
-  ) %>%
-  filter(
-    gen == 1000,
-    between(ploidy_rate, 0, 0.25)  
-  ) %>%
-  ggplot(aes(x = ploidy_rate, y = count, colour = subset)) +
-  scale_colour_manual(values = ploidylvl_colours) +
-  geom_vline(xintercept = 0.171, color = "red", size = .2) +
-  geom_point(alpha = .2) +
-  theme_classic() +
-  theme(
-    legend.position = "none",
-    axis.title = element_text(size = 7, face = "bold"),
-    plot.background = element_rect(fill = "#efefef"),
-    strip.background = element_rect(colour = "white"),
-    strip.text = element_text(size = 7, face = "bold")
-  ) +
-  xlab("Rate of genome-doubling") +
-  ylab("Frequency of adults in population") +
-  scale_x_continuous(
-    breaks = seq(0, .2, .1)
-    #limits = c(0, .25)
-  ) +
-  scale_y_continuous(
-    breaks = seq(0, 1, .5),
-    limits = c(0, 1.01)
-  )
